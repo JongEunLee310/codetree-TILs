6f7009a6e284637771ff8f13e75956779f42ba5e
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-10 16:27:46 +0900

    Title: L, R 그리고 B, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240310/L, R 그리고 B/l-r-and-b.py
+++ 240310/L, R 그리고 B/l-r-and-b.py
@@ -14,8 +14,16 @@ for i in range(10):
             R.append(j)
 
 result = 0
-if B[0] == R[0] and L[0] == R[0] or B[1] == R[1] and L[1] == R[1]:
-    result = abs(B[0] - L[0]) + abs(B[1] - L[1]) + 1
+if B[0] == R[0] and L[0] == R[0]:
+    if B[1] < R[1] and R[1] < L[1]:
+        result = abs(B[0] - L[0]) + abs(B[1] - L[1]) + 1
+    else:
+        result = abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1
+elif B[1] == R[1] and L[1] == R[1]:
+    if B[0] < R[0] and R[0] < L[0]:
+        result = abs(B[0] - L[0]) + abs(B[1] - L[1]) + 1
+    else:
+        result = abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1
 else:
     result = abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1
 print(result)
