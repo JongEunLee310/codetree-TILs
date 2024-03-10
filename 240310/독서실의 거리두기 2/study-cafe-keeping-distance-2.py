6d56c5f68b21641874bd2ec1fffada6ad7666b39
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-10 14:36:41 +0900

    Title: 독서실의 거리두기 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240310/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -52,15 +52,15 @@ if len(max_loc) == 0:
     idx_1 = line.index('1')
     result = max(idx_1, N - idx_1 - 1)
 else:
-    max_dist_center_idx = (max_loc[0][1] - max_loc[0][0]) // 2
+    max_dist_center_idx = (max_loc[0][1] + max_loc[0][0]) // 2
     line[max_dist_center_idx] = '1'
     result = get_min_dist(line, N)
     line[max_dist_center_idx] = '0'
 
     if line[0] == '0':
-        line[0] == '1'
+        line[0] = '1'
         result = max(result, get_min_dist(line, N))
-        line[0] == '0'
+        line[0] = '0'
     
     if line[-1] == '0':
         line[-1] = '1'
