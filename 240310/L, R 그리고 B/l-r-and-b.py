181eeb2c082c75613fd8ca1a6288054f01664b0a
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-10 16:32:53 +0900

    Title: L, R 그리고 B, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/L, R 그리고 B/l-r-and-b.py
+++ 240310/L, R 그리고 B/l-r-and-b.py
@@ -15,14 +15,14 @@ for i in range(10):
 
 result = 0
 if B[0] == R[0] and L[0] == R[0]:
-    if B[1] < R[1] and R[1] < L[1]:
+    if B[1] < R[1] and R[1] < L[1] or L[1] < R[1] and R[1] < B[1]:  # B, L, R이 일직선 상에 있을 때 R이 B, L 사이에 있으면 우회
         result = abs(B[0] - L[0]) + abs(B[1] - L[1]) + 1
-    else:
+    else:     # B, L, R이 일직선 상에 있을 때 R이 B, L 사이에 없으면 직행
         result = abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1
 elif B[1] == R[1] and L[1] == R[1]:
-    if B[0] < R[0] and R[0] < L[0]:
+    if B[0] < R[0] and R[0] < L[0] or L[0] < R[0] and R[0] < B[0]:  # B, L, R이 일직선 상에 있을 때 R이 B, L 사이에 있으면 우회
         result = abs(B[0] - L[0]) + abs(B[1] - L[1]) + 1
-    else:
+    else:   # B, L, R이 일직선 상에 있을 때 R이 B, L 사이에 없으면 직행
         result = abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1
 else:
     result = abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1
