02e60f125a46c8a7185a4976009e29421043ffdf
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-11 21:53:07 +0900

    Title: A, B, C, D 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240311/A, B, C, D 찾기/find-a-b-c-d.py
@@ -0,0 +1,14 @@
+nums = sorted([int(x) for x in input().split()])
+
+A, B = nums[0], nums[1]
+C, D = 0, 0
+for i in range(2, len(nums)):
+    if nums[i] <= A + B:
+        C = nums[i]
+        D = nums[-1] - A - B - C
+        if not nums.index(D):
+            C, D = 0, 0
+        else:
+            break
+
+print(A, B, C, D)
