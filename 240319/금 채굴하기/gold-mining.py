89b8ad1d1c308dd7b88cea4cd58ca22c83271983
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-19 21:31:01 +0900

    Title: 금 채굴하기, Time: 202ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240319/금 채굴하기/gold-mining.py
+++ 240319/금 채굴하기/gold-mining.py
@@ -7,7 +7,7 @@ grid = [[int(x) for x in input().split()] for _ in range(n)]
 # k를 기준으로 완점 탐색
 max_profit_gold = 0
 for k in range(n):
-    cost = k * k + (k + 1) * (k + 1)
+    cost = 1 if k == 0 else k * k + (k + 1) * (k + 1)
     for i in range(n):
         for j in range(n):
             gold = 0
@@ -15,7 +15,7 @@ for k in range(n):
                 for b in range(j - k, j + k + 1):
                     if in_range(a, b, n, n) and abs(i - a) + abs(j - b) <= k and grid[a][b] == 1:
                         gold += 1
-            if gold * m > cost:
+            if gold * m >= cost:
                 max_profit_gold = max(max_profit_gold, gold)
 
 print(max_profit_gold)
