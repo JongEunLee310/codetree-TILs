4c376505d15594314ca1935a467855096e2c5235
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-22 22:49:36 +0900

    Title: 대폭발, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/대폭발/big-explosion.py
+++ 240322/대폭발/big-explosion.py
@@ -5,9 +5,12 @@ def explode(grid, t):
     dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
     dist = 2 ** (t - 1)
 
+    # grid 전체를 순회하면서 이전에 배치한 폭탄이 있을 때 지정된 거리에 해당하는 위치에 새로운 폭탄을 설치
     for i in range(len(grid)):
         for j in range(len(grid)):
+            # 이전 시점에 설치된 폭탄 탐색
             if grid[i][j] != None and grid[i][j] < t:
+                # 격자 내에서 상하좌우 모두 지정된 거리에 해당하는 위치에 폭탄 설치
                 for k in range(4):
                     nx, ny = j + dx[k] * dist, i + dy[k] * dist
                     if in_range(nx, ny, len(grid), len(grid)) and grid[ny][nx] == None:
