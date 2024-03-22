03f5cc05efc67ffe2fc3a9a8fcdfc6c7ce7bd527
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-22 18:10:55 +0900

    Title: 떨어지는 1자 블록, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240322/떨어지는 1자 블록/falling-horizontal-block.py
@@ -4,17 +4,17 @@ def in_range(x, n):
 n, m, k = [int(x) for x in input().split()]
 grid = [[int(x) for x in input().split()] for _ in range(n)]
 
+stop = False
 for i in range(n):
-    stop_loc = False
     # 블럭 아래에 붙은 블럭이 있는지 검사
-    if i + 1 < n and not stop_loc:
+    if i + 1 < n:
         for j in range(k - 1, k + m - 1):
             if grid[i + 1][j] == 1:
-                stop_loc = i
+                stop = True
                 break
     
-    # 블럭이 바닥까지 내려왔을 경우
-    if stop_loc != False or not stop_loc and i + 1 == n:
+    # 정지위치가 정해졌거나 블럭이 바닥까지 내려왔을 경우 블럭을 격자에 위치를 확정
+    if stop or not stop and i + 1 == n:
         for j in range(k - 1, k + m - 1):
             grid[i][j] = 1
         break
