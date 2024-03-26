4643d129dfe480f64e5baaac286615b5e3223a4a
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-26 23:23:22 +0900

    Title: 최소 점프 횟수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/최소 점프 횟수/min-num-of-jumps.py
+++ 240326/최소 점프 횟수/min-num-of-jumps.py
@@ -3,6 +3,7 @@ from sys import maxsize
 # n번째 위치에 도달하기 위한 최소 점프 횟수를 구하는 함수
 def min_jump(cur, cnt, n, j):
     if cur == n: return cnt
+    if cur > n: return maxsize
 
     min_cnt = maxsize
     for i in range(1, j[cur] + 1):
