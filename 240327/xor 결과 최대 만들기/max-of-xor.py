878df692f03dcf9cd9a6db9b04ab5cc6d2012347
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-27 15:36:03 +0900

    Title: xor 결과 최대 만들기, Time: 370ms, Memory: 73MB, Status: Passed - CodeTree

--- 240327/xor 결과 최대 만들기/max-of-xor.py
+++ 240327/xor 결과 최대 만들기/max-of-xor.py
@@ -1,14 +1,28 @@
 # 재귀적으로 m번의 xor 결과 중 최대 값을 찾는 함수
-def max_xor(nums, m, idx, cnt, result):
-    if cnt == m: return result
+def max_xor(nums, m, idx, cnt, l):
+    if cnt == m: return xor(l)
     if idx >= len(nums):
-        if cnt == m: return result
+        if cnt == m: return xor(l)
         else: return -1
 
-    max_result = max(result, max_xor(nums, m, idx + 1, cnt + 1, result ^ nums[idx]), max_xor(nums, m, idx + 1, cnt, result)) if result != 0 else max(max_xor(nums, m, idx + 1, cnt + 1, result ^ nums[idx]), max_xor(nums, m, idx + 1, cnt, result))
+    max_result = 0
+    # 현재 인덱스의 수를 포함한 xor 결과값
+    l.append(nums[idx])
+    r1 = max_xor(nums, m, idx + 1, cnt + 1, l)
+    l.pop()
+    # 현재 인덱스의 수를 제외한 xor 결과값
+    r2 = max_xor(nums, m, idx + 1, cnt, l)
 
+    max_result = max(max_result, r1, r2)
     return max_result
+
+# xor 수행 함수
+def xor(l):
+    result = 0
+    for n in l:
+        result ^= n
+    return result
 
 n, m = [int(x) for x in input().split()]
 nums = [int(x) for x in input().split()]
-print(max_xor(nums, m, 0, 0, 0))
+print(max_xor(nums, m, 0, 0, []))
