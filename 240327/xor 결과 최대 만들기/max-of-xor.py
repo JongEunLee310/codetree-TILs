d082388f89db9c4d7cc170383413479c1ca42697
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-27 15:28:51 +0900

    Title: xor 결과 최대 만들기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240327/xor 결과 최대 만들기/max-of-xor.py
+++ 240327/xor 결과 최대 만들기/max-of-xor.py
@@ -1,13 +1,14 @@
 # 재귀적으로 m번의 xor 결과 중 최대 값을 찾는 함수
-def max_xor(nums, m, cnt, result):
+def max_xor(nums, m, idx, cnt, result):
     if cnt == m: return result
+    if idx >= len(nums):
+        if cnt == m: return result
+        else: return -1
 
-    max_result = 0
-    for i in nums:
-        max_result = max(max_result, max_xor(nums, m, cnt + 1, i if result == 0 else result ^ i))
-    
+    max_result = max(result, max_xor(nums, m, idx + 1, cnt + 1, result ^ nums[idx]), max_xor(nums, m, idx + 1, cnt, result))
+
     return max_result
 
 n, m = [int(x) for x in input().split()]
 nums = [int(x) for x in input().split()]
-print(max_xor(nums, m, 0, 0))
+print(max_xor(nums, m, 0, 0, 0))
