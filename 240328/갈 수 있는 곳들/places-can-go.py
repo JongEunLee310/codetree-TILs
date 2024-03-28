4f70121c386f0c81f94043ca28866f32db88054c
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-28 15:50:33 +0900

    Title: 갈 수 있는 곳들, Time: 223ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240328/갈 수 있는 곳들/places-can-go.py
+++ 240328/갈 수 있는 곳들/places-can-go.py
@@ -5,13 +5,13 @@ def in_range(x, y, n, m):
 
 def can_go(x, y, v, g):
     if not in_range(x, y, len(g[0]), len(g)): return False
-    if v[y][x] or g[y][x] == 1: return False
+    if v[y][x] == 1 or g[y][x] == 1: return False
 
     return True
 
 def push(x, y, v, q):
     q.append([y, x])
-    v[y][x] = True
+    v[y][x] = 1
 
 # bfs를 사용해서 시작점으로부터 갈 수 있는 지점의 개수를 구하는 함수
 def bfs(g, v, dxs, dys, q):
@@ -24,32 +24,22 @@ def bfs(g, v, dxs, dys, q):
             if can_go(nx, ny, v, g):
                 push(nx, ny, v, q)
 
-
 n, k = [int(x) for x in input().split()]
 grid = [[int(x) for x in input().split()] for _ in range(n)]
 starts = [[int(x) - 1 for x in input().split()] for _ in range(k)]
 
-# 모든 시작점에서 도달할 수 있는 지점을 방문한 여부를 저장하는 리스트
-total_visited = [[0 for _ in range(n)] for _ in range(n)]
-
+q = deque()
 dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
-
-# 시작점의 개수만큼 bfs 실행
+# 방문 여부를 저장하는 visited 리스트 초기화
 for r, c in starts:
-    # 방문 여부를 저장하는 visited 리스트 초기화
-    visited = [[False for _ in range(n)] for _ in range(n)]
+    visited = [[0 for _ in range(n)] for _ in range(n)]
     visited[r][c] = True
 
-    q = deque()
     q.append([r, c])
     
-    bfs(grid, visited, dx, dy, q)
-
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j]: total_visited[i][j] = 1
+bfs(grid, visited, dx, dy, q)
 
 result = 0
 for i in range(n):
-    result += sum(total_visited[i])
+    result += sum(visited[i])
 print(result)
