b8e8fcbc39add3e1377a5d0455e9d18544bb1d65
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-30 14:10:47 +0900

    Title: 피보나치 수, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240330/피보나치 수/fibonacci-number.py
+++ 240330/피보나치 수/fibonacci-number.py
@@ -1,11 +1,12 @@
 def fibo(n, m):
-    if n <= 2:
-        if m[n] == 0:
-            m[n] = 1
-        return m[n]
- 
-    return m[n] if m[n] != 0 else fibo(n - 1, m) + fibo(n - 2, m)
+    if n < 2: return m[n]
+
+    if m[n] == 0:
+        m[n] = fibo(n - 1, m) + fibo(n - 2, m)
+    
+    return m[n]
 
 n = int(input())
-memo = [0 for i in range(n + 1)]
+memo = [0 for _ in range(n + 1)]
+memo[1] = 1
 print(fibo(n, memo))
