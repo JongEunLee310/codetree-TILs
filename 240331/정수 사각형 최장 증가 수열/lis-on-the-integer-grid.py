374717a78106f53772b7efd66ed730d79c3fe257
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-03-31 16:58:57 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 548ms, Memory: 61MB, Status: Passed - CodeTree

--- 240331/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240331/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -20,13 +20,13 @@ grid = [[int(x) for x in input().split()] for _ in range(n)]
 cell = init_cell(n, grid)
 dp = init_dp(n, grid)
 
-# 현재 검사하는 칸에 인접한 칸 중 현재 칸의 값보다 큰 칸 중 최대값에 현재 거리에 1 더해준다.
+# 현재 검사하는 칸에 인접한 칸 중 현재 칸의 값보다 큰 칸 중 최대값에 현재 거리에 1 더해준다. 혹은 현재 거리에 1 더해준 값보다 인접한 칸의 dp 테이블 값이 더 크다면 갱신하지 않는다.
 dxs, dys = [0, 1, 0, -1], [-1, 0, 1, 0]
 for v, y, x in cell:
     for dx, dy in zip(dxs, dys):
         nx, ny = x + dx, y + dy
         if in_range(nx, ny, n, n) and grid[ny][nx] > grid[y][x]:
-            dp[ny][nx] = dp[y][x] + 1
+            dp[ny][nx] = max(dp[ny][nx], dp[y][x] + 1)
 
 max_dist = 0
 for i in range(n):
