5ea68a8e2e9b80a9398139bd9aab4a22635dc2b3
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-03 17:23:34 +0900

    Title: 2차원 최대 증가 수열, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240403/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,17 @@
+n, m = [int(x) for x in input().split()]
+grid = [[int(x) for x in input().split()] for _ in range(n)]
+
+dp =[[0 for _ in range(m)] for _ in range(n)]
+dp[0][0] = 1
+for i in range(1, n):
+    for j in range(1, m):
+        for k in range(i):
+            for l in range(j):
+                if grid[i][j] > grid[k][l]:
+                    dp[i][j] = max(dp[i][j], dp[k][l] + 1)
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        ans = max(ans, dp[i][j])
+print(ans)
