5c3e516b84491d15bbc68af63ca30ba5cbeedcee
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-03 17:47:33 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240403/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240403/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -3,11 +3,11 @@ def in_range(l1, l2):
 
 n = int(input())
 lines = [[int(x) for x in input().split()] for _ in range(n)]
-dp = [0 for _ in range(n)]
+lines.sort(key = lambda x : x[1])
+dp = [1 for _ in range(n)]
 for i in range(n):
     for j in range(i):
         if not in_range(lines[i], lines[j]):
             dp[i] = max(dp[i], dp[j] + 1)
-    if dp[i] == 0: dp[i] = 1
 
 print(max(dp))
