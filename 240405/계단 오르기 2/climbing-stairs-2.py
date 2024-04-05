fca6b659507475933f4423af278bcac69cb192be
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-05 15:19:50 +0900

    Title: 계단 오르기 2, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240405/계단 오르기 2/climbing-stairs-2.py
@@ -0,0 +1,19 @@
+from sys import maxsize
+
+def init_dp(n):
+    dp = [[-maxsize for _ in range(4)] for _ in range(n + 1)]
+    for i in range(n + 1):
+        dp[i][0] = 0
+    return dp
+
+n = int(input())
+coins = [int(x) for x in input().split()]
+
+# n층까지 1계단 오르는 것을 3번 이내로 수행한 경우을 고려
+    # dp[i - 2][j] + coins[i]과 dp[i - 1][j - 1] + coins[i] 중 최대값을 dp[i][j]에 저장
+dp = init_dp(n)
+for i in range(1, n + 1):
+    for j in range(1, min(4, i + 1)):
+        dp[i][j] = max(dp[i][j], dp[i - 2][j] + coins[i - 1], dp[i - 1][j - 1] + coins[i - 1]) if i > 2 else max(dp[i][j], dp[i - 1][j - 1] + coins[i - 1])
+ 
+print(max(dp[n]))
