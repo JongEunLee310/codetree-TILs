38de0bd493e7a5577f7516d1f411194c0c260d89
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-05 18:42:24 +0900

    Title: 고대 보물 지도의 비밀, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240405/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 240405/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -21,9 +21,6 @@ for i in range(1, n + 1):
     # 추가되는 수가 음수 일 때
         # 큐에 음수가 추가되었을 때의 dp테이블 인덱스를 추가하고 현재 음수 개수를 1 증가
     if nums[i - 1] < 0:
-        q.append(i)
-        cur_k += 1
-
         # 현재 음수 개수가 k개 이하일 때
             # 직전 최대 부분 연속합과 음수의 합이 음수보다 작으면
                 # dp[i]는 새로운 음수값
@@ -35,24 +32,30 @@ for i in range(1, n + 1):
             if dp[i - 1] + nums[i - 1] < nums[i - 1]:
                 dp[i] = nums[i - 1]
                 q.clear()
-                q.append(i)
+                q.append([i, dp[i]])
                 cur_k = 1
             else:
                 dp[i] = dp[i - 1] + nums[i - 1]
+                q.append([i, dp[i]])
+                cur_k += 1
 
         # 현재 음수 개수가 k개 초과일 때
             # 가장 먼저 등장하는 음수까지의 최대 연속 부분합을 직전 연속 최대 부분합에서 뺀 값을 tmp로 사용
                 # tmp에 새로운 음수를 더한 값과 새로운 음수 중 최대값을 dp[i]에 저장
         else:
-            front_odd = q.popleft()
-            tmp = dp[i - 1] - dp[front_odd]
+            front_odd_idx, v = q.popleft()
+            tmp = dp[i - 1] - v
             if tmp + nums[i - 1] < nums[i - 1]:
                 dp[i] = nums[i - 1]
                 q.clear()
-                q.append(i)
+                q.append([i, dp[i]])
                 cur_k = 1
             else:
                 dp[i] = tmp + nums[i - 1]
+                for j in range(len(q)):
+                    q[j][1] -= v
+                q.append([i, dp[i]])
     else:
         dp[i] = max(dp[i - 1] + nums[i - 1], nums[i - 1])
+
 print(max(dp))
