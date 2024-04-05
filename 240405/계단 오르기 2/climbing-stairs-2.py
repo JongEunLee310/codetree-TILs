2054335de6b69d069352e9e394fcc1c06a154ff0
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-05 15:35:54 +0900

    Title: 계단 오르기 2, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240405/계단 오르기 2/climbing-stairs-2.py
+++ 240405/계단 오르기 2/climbing-stairs-2.py
@@ -1,9 +1,9 @@
 from sys import maxsize
 
-def init_dp(n):
-    dp = [[-maxsize for _ in range(4)] for _ in range(n + 1)]
-    for i in range(n + 1):
-        dp[i][0] = 0
+def init_dp(n, c):
+    dp = [[0 for _ in range(4)] for _ in range(n + 1)]
+    for i in range(2, n + 1, 2):
+        dp[i][0] = coins[i - 1]
     return dp
 
 n = int(input())
@@ -11,7 +11,7 @@ coins = [int(x) for x in input().split()]
 
 # n층까지 1계단 오르는 것을 3번 이내로 수행한 경우을 고려
     # dp[i - 2][j] + coins[i]과 dp[i - 1][j - 1] + coins[i] 중 최대값을 dp[i][j]에 저장
-dp = init_dp(n)
+dp = init_dp(n, coins)
 for i in range(1, n + 1):
     for j in range(1, min(4, i + 1)):
         dp[i][j] = max(dp[i][j], dp[i - 2][j] + coins[i - 1], dp[i - 1][j - 1] + coins[i - 1]) if i - j >= 2 else max(dp[i][j], dp[i - 1][j - 1] + coins[i - 1])
