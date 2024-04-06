fc3f97d7bb31f13ea7ebd4ede5a51c11d1084654
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-06 21:15:59 +0900

    Title: 신전 탐험하기 2, Time: 192ms, Memory: 26MB, Status: Passed - CodeTree

--- 240406/신전 탐험하기 2/explore-temple-2.py
+++ 240406/신전 탐험하기 2/explore-temple-2.py
@@ -1,34 +1,33 @@
 def init_dp(n, f_info):
-    # dp 테이블에 1층에서 선택했던 방의 정보를 기억하도록 초기화
-    dp = [[[0, -1] for _ in range(3)] for _ in range(n)]
+    # 1층에서 선택한 방에서 출발하는 경우로 나눠 초기화 dp[0] = 왼쪽, dp[1] = 가운데, dp[2] = 오른쪽에서 출발
+    dp = [[[0 for _ in range(3)] for _ in range(n)] for _ in range(3)]
     # dp의 첫번째 줄은 1층의 보물 정보로 초기화
     for i in range(3):
-        dp[0][i][0] = f_info[i]
-        dp[0][i][1] = i
+        dp[i][0][i] = f_info[i]
     return dp
 
 n = int(input())
 floor_info = [[int(x) for x in input().split()] for _ in range(n)]
 
+# 1층에서 왼쪽, 중간, 오른쪽에서 출발하는 경우 세가지로 나눈다.
 # dp[i][j]으로 올 수 있는 방 중 직전까지 모든 보물의 최대값을 선택하는 dp
 dp = init_dp(n, floor_info[0])
-for i in range(1, n):
-    # i층의 j번째 방이 선택할 수 있는 최대값을 탐색
-    for j in range(3):
-        for k in range(3):
-            # 현재 선택한 방과 직전에 선택한 방이 같으면 무시
-            if j == k:
-                continue
 
-            if dp[i][j][0] <= dp[i - 1][k][0] + floor_info[i][j]:
-                dp[i][j][0] = dp[i - 1][k][0] + floor_info[i][j]
-                dp[i][j][1] = dp[i - 1][k][1]
+for s in range(3):
+    # 1층의 s번째 방에서 출발하는 경우의 dp 테이블을 채움
+    for i in range(1, n):
+        # i층의 j번째 방이 선택할 수 있는 최대값을 탐색
+        for j in range(3):
+            for k in range(3):
+                # 현재 선택한 방과 직전에 선택한 방이 같으면 무시
+                if j == k or i == n - 1 and j == s:
+                    continue
 
-# n층의 값 중 1층에서 선택한 방과 n층에서 선택한 방의 위치가 다른 것 중 최대값이 정답
+                dp[s][i][j] = max(dp[s][i][j], dp[s][i - 1][k] + floor_info[i][j])
+
+# 서로 다른 방에서 출발한 경우에서 n층의 값 중 1층에서 선택한 방과 n층에서 선택한 방의 위치가 다른 것 중 최대값이 정답
 ans = 0
 for i in range(3):
-    if dp[-1][i][1] == i:
-        continue
-    ans = max(ans, dp[-1][i][0])
+    ans = max(ans, max(dp[i][-1]))
 
 print(ans)
