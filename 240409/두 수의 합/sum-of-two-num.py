38c5acbdcadb275f21dc5d13c4904a5c759db8ec
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-09 15:14:51 +0900

    Title: 두 수의 합, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240409/두 수의 합/sum-of-two-num.py
+++ 240409/두 수의 합/sum-of-two-num.py
@@ -1,12 +1,9 @@
 n, k = [int(x) for x in input().split()]
 nums = [int(x) for x in input().split()]
 
-d = {}
-for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
-        try:
-            d[nums[i] + nums[j]] += 1
-        except:
-            d[nums[i] + nums[j]] = 1
-
-print(d[k])
+d = {}.fromkeys(nums, 0)
+cnt = 0
+for i in range(n):
+    if k - nums[i] in d:
+        cnt += 1
+print(cnt // 2)
