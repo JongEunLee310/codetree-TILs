ad00353f946af1e1d0daa6aefe6f6b4c84c14d23
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-09 16:15:43 +0900

    Title: 두 수의 합, Time: 175ms, Memory: 35MB, Status: Passed - CodeTree

--- 240409/두 수의 합/sum-of-two-num.py
+++ 240409/두 수의 합/sum-of-two-num.py
@@ -1,18 +1,18 @@
 n, k = [int(x) for x in input().split()]
 nums = [int(x) for x in input().split()]
 
-d = {}
-for i in range(n):
-    try:
-        d[nums[i]][i] = True
-    except:
-        d[nums[i]] = {i : True}
-
+d = {}.fromkeys(nums, 0)
 
 cnt = 0
 for i in range(n):
-    for j in range(i + 1, n):
-        if k - nums[i] in d and j in d[k - nums[i]]:
-            cnt += 1
-            
+    # t는 현재 숫자가 k가 되기 위한 숫자를 의미
+    t = k - nums[i]
+    # t가 d에 있다면 cnt에 현재 인덱스까지 카운트한 t의 개수를 더해준다.
+    if t in d:
+       cnt += d[t]
+
+    # 현재 숫자가 d에 있을 때 1 추가, 없으면 1로 초기화
+    if nums[i] in d:
+        d[nums[i]] += 1
+ 
 print(cnt)
