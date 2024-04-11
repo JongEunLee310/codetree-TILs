2b8be69c14bd936654e9b2e850e99f4ca992e4ef
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-11 12:58:21 +0900

    Title: 데이터 비교, Time: 238ms, Memory: 35MB, Status: Passed - CodeTree

---
+++ 240411/데이터 비교/data-comparison.py
@@ -0,0 +1,7 @@
+n = int(input())
+s1 = set([int(x) for x in input().split()])
+m = int(input())
+nums = [int(x) for x in input().split()]
+
+for num in nums:
+    print(1 if num in s1 else 0, end=' ')
