e414e93cfc1020637a627ed3a9d6c0838bea9400
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-12 17:04:17 +0900

    Title: 자리 차지하기, Time: 677ms, Memory: 33MB, Status: Wrong Answer - CodeTree

--- 240412/자리 차지하기/take-place.py
+++ 240412/자리 차지하기/take-place.py
@@ -1,12 +1,12 @@
 from sortedcontainers import SortedSet
 
 n, m = [int(x) for x in input().split()]
-chair = [int(x) - 1 for x in input().split()]
-s = SortedSet()
+chair = [int(x) for x in input().split()]
+s = SortedSet([0])
 for c in chair:
     lower_bound = s.bisect_left(c)
-    if lower_bound <= c:
+    if lower_bound <= c and lower_bound <= m:
         s.add(c)
     else:
         break
-print(len(s))
+print(len(s) - 1)
