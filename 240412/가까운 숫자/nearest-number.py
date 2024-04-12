33c0b1b6256354ca2eeb500b43bb159ccecd8a5f
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-12 16:33:55 +0900

    Title: 가까운 숫자, Time: 1828ms, Memory: 40MB, Status: Passed - CodeTree

--- 240412/가까운 숫자/nearest-number.py
+++ 240412/가까운 숫자/nearest-number.py
@@ -3,12 +3,20 @@ from sys import maxsize
 
 n = int(input())
 dots = [int(x) for x in input().split()]
+
 s = SortedSet([0])
+min_dist = maxsize
 for i in range(n):
     s.add(dots[i])
-    min_dist = maxsize
-    for j in range(len(s)):
-        upper_bound = s.bisect_right(s[j])
-        dist = s[upper_bound] - s[j] if upper_bound < len(s) else maxsize
-        min_dist = min(min_dist, dist)
+    
+    # 입력된 숫자의 위치를 기준으로 왼쪽으로 가장 가까운 숫자와 거리를 구해서 min_dist와 비교
+    lower_bound = s.bisect_left(dots[i])
+    dist1 = dots[i] - s[lower_bound - 1] if lower_bound < len(s) else maxsize
+    min_dist = min(min_dist, dist1)
+
+    # 입력된 숫자의 위치를 기준으로 오른쪽으로 가장 가까운 숫자와 거리를 구해서 min_dist와 비교
+    upper_bound = s.bisect_right(dots[i])
+    dist2 = s[upper_bound] - dots[i] if upper_bound < len(s) else maxsize
+    min_dist = min(min_dist,dist2)
+
     print(min_dist)
