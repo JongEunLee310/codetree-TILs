e6c84d4cc86b85cfb60ad114a8cc42d385208589
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-15 13:49:18 +0900

    Title: 마지막으로 남은 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240415/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,16 @@
+import heapq
+
+n = int(input())
+pq = [-int(x) for x in input().split()]
+heapq.heapify(pq)
+
+while len(pq) >= 2:
+    n1 = heapq.heappop(pq)
+    n2 = heapq.heappop(pq)
+
+    diff = abs(n1 - n2)
+    if diff == 0:
+        continue
+    heapq.heappush(pq, -diff)
+
+print(-pq[0])
