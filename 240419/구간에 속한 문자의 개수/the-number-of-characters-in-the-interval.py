dbbf713bd179883caa7700d3752302fe70040814
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2024-04-19 13:28:00 +0900

    Title: 구간에 속한 문자의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240419/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 240419/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -6,7 +6,7 @@ prefix_sum = [[[0 for _ in range(n + 1)] for _ in range(n + 1)] for _ in range(3
 for i in range(3):
     cur = chr(ord('a') + i)
     for j in range(1, n + 1):
-        for l in range(1, n + 1):
+        for l in range(1, m + 1):
             # 이전까지 고려한 누적합의 합에서 중복을 제거
             prefix_sum[i][j][l] = prefix_sum[i][j - 1][l] + prefix_sum[i][j][l - 1] - prefix_sum[i][j - 1][l - 1]
             # 현재 검사 중인 격자칸의 알파벳이 cur과 같을 때 현재까지 누적합에 1 추가
