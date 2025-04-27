faa8b7e716aa8ab715908ed717d5b7d59dfb262b
SigningOwl <63397254+SighingOwl@users.noreply.github.com>
2025-04-28 00:06:03 +0900

    Title: 달에서 무게 구하기, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250428/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double pendulum = 13;
+        double gravity = 0.165;
+
+        System.out.printf("%.0f", pendulum);
+        System.out.printf(" * ");
+        System.out.printf("%.6f", gravity);
+        System.out.printf(" = ");
+        System.out.printf("%.6f", pendulum * gravity);
+    }
+}
