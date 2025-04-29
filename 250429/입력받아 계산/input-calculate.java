8a6ddbe92181cb7942aa56e77e03cefcbce866bf
SighingOwl <63397254+SighingOwl@users.noreply.github.com>
2025-04-30 07:43:40 +0900

    Title: 입력받아 계산, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250429/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num + 2);
+    }
+}
