d3c74efed37b34bf8cef7b262dfc09ffef55e924
JongEunLee <63397254+JongEunLee310@users.noreply.github.com>
2025-07-27 16:02:39 +0900

    Title: 일의 자리 배열, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250727/일의 자리 배열/array-with-units.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            if (i < 2) {
+                nums[i] = sc.nextInt();
+                continue;
+            }
+
+            nums[i] = (nums[i - 1] + nums[i - 2]) % 10;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(nums[i] + " ")
+        }
+    }
+}
