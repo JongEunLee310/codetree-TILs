3dd99c139cbec3327ba90527cb4d875db280e380
JongEunLee <63397254+JongEunLee310@users.noreply.github.com>
2025-07-27 16:03:09 +0900

    Title: 일의 자리 배열, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

--- 250727/일의 자리 배열/array-with-units.java
+++ 250727/일의 자리 배열/array-with-units.java
@@ -15,7 +15,7 @@ public class Main {
         }
 
         for (int i = 0; i < 10; i++) {
-            System.out.print(nums[i] + " ")
+            System.out.print(nums[i] + " ");
         }
     }
 }
