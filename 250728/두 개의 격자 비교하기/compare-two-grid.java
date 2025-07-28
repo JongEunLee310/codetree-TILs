a03e36f12e4bbc511dfc3fe94d1b9f9019d21ac6
JongEunLee <63397254+JongEunLee310@users.noreply.github.com>
2025-07-28 18:21:57 +0900

    Title: 두 개의 격자 비교하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250728/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] matrixA = new int[N][M];
+        int[][] matrixB = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                matrixA[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                matrixB[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (matrixA[i][j] == matrixB[i][j]) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(1 + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
