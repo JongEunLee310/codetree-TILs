3333c721314f8c7a577d8f8c8c0bab3d67e151ae
JongEunLee <63397254+JongEunLee310@users.noreply.github.com>
2025-07-28 11:29:42 +0900

    Title: 친근하지 않은 수, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250728/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int unfriendly = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+                continue;
+            }
+
+            unfriendly++;
+        }
+
+        System.out.print(unfriendly);
+    }
+}
