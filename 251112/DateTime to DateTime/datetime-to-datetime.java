cf3bc64a30d836ebd05ea7835144391b39b2d35f
JongEunLee <63397254+JongEunLee310@users.noreply.github.com>
2025-11-12 20:59:56 +0900

    Title: DateTime to DateTime, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251112/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        int diff = (A * 24 * 60 + B * 60 + C) - (11 * 24 * 60 + 11 * 60 + 11);
+
+        if (diff < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(diff);
+        }
+        
+    }
+}
