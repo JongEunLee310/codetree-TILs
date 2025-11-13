526ff1183e22036f007eacbca4456b05c5e10a82
JongEunLee <63397254+JongEunLee310@users.noreply.github.com>
2025-11-13 21:46:04 +0900

    Title: 흰검 칠하기, Time: 129ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251113/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] w_tiles = new int[31];
+        int[] b_tiles = new int[31];
+        Integer[] tiles = new Integer[31];
+        int curr = 15;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            for (int j = 0; j < x; j++) {
+                if (d == 'L') {
+                    if (j > 0) curr--;
+                    w_tiles[curr]++;
+                    if (w_tiles[curr] >= 2 && b_tiles[curr] >= 2) {
+                        tiles[curr] = 3;
+                    } else {
+                        tiles[curr] = 1;
+                    }
+                } else {
+                    if (j > 0) curr++;
+                    b_tiles[curr]++;
+                    if (b_tiles[curr] >= 2 && w_tiles[curr] >= 2) {
+                        tiles[curr] = 3;
+                    } else {
+                        tiles[curr] = 2;
+                    }
+                }
+            }
+        }
+
+        int w = 0;
+        int b = 0;
+        int g = 0;
+        for (int i = 0; i < 31; i++) {
+            if (tiles[i] == null) {
+                continue;
+            } else if (tiles[i] == 1) {
+                w++;
+            } else if (tiles[i] == 2) {
+                b++;
+            } else {
+                g++;
+            }
+        }
+
+        System.out.println(w + " " + b + " " + g);
+    }
+}
